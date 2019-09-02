70517dd577819dcef57cd70abe3c42960f14b298
skens06 <kenny.moutote@coding-academy.fr>
2019-09-02 16:40:07 +0200

    create user model

---
+++ server/models/user.model.js
@@ -0,0 +1,82 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+    username: {
+        type: String,
+        min: 3,
+        max: 15,
+        unique: true,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        validate: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+
+    birthdate: {
+        type: Number,
+        required: true,
+    },
+
+    admin: {
+        type: Boolean,
+        default: false
+    },
+
+    banned: {
+        status: {
+            type: Boolean,
+            default: false
+        },
+        Date: {
+            type: Date
+        }
+    },
+
+    suspended: {
+        type: Boolean,
+        default: false
+    },
+
+    avatar: {
+        type: String
+    },
+
+    description: {
+        type: String
+    },
+
+    website: {
+        type: String,
+        validate: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
+    },
+
+    language: {
+        type: String
+    },
+
+
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
