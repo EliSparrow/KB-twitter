d165423c08d7d3bf7bf7a803147a5933c171c188
skens06 <kenny.moutote@coding-academy.fr>
2019-09-02 16:38:46 +0200

    create post model

---
+++ server/models/post.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let PostSchema = new Schema ({
+
+    content: {
+        type: String,
+        max: 140,
+        required: true
+    },
+
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+
+    },
+
+    createAt: {
+        type: Date,
+        default: Date.now
+    },
+
+    updateAt: {
+        type: Date,
+        default: Date.now
+    },
+}),
+
+
+const Post = mongoose.model("Post", PostSchema);
+module.exports = Post;
