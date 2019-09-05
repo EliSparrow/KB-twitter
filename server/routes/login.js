de753ff4919e6cf4ad7985fb7906a1e0adc30eea
skens06 <kenny.moutote@coding-academy.fr>
2019-09-05 15:07:04 +0200

    new package + login route + register route

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,53 +1,49 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const auth = require('../middleware/auth');
+const User = require('../models/user.model');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
-const gravatar = require('gravatar');
-const User = require('../models/user.model');
+const jwt = require('jsonwebtoken');
+
+
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
 
 router.post('/', [
-    check('username', 'Username is required')
-        .not()
-        .isEmpty(),
+
     check('email', 'Email not valid').isEmail(),
-    check('password', 'Password must be 5 or more characters').isLenght({ min: 5 }),
-    check('birthdate', 'Enter date in format only').isISO8601()
+    check('password', 'Password is required').exists()
 ],
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ erros: errors.array() });
         }
-        const { username, email, password } = req.body;
+        const { email, password } = req.body;
 
         try {
             let user = await User.findOne({ email });
 
-            if (user) {
-               return res.status(400).json({ errors: [{ message: 'Email already exist' }] });
+            if (!user) {
+               return res.status(400).json({ errors: [{ message: 'Invalid info' }] });
             }
 
-            const avatar = gravatar.url(email, {
-                s: '200',
-                r: 'pg',
-                d: 'mm'
-            })
+            const isMatch = await bcrypt.compare(password, user.password);
 
-            user = new User({
-                username,
-                email,
-                password,
-                avatar
-            });
-
-            const salt = await bcrypt.genSalt(10);
-
-            user.password = await bcrypt.hash(password, salt);
-
-            await user.save();
-
+            if (!isMatch){
+                return res.status(400).json({ errors: [{ message: 'Invalid info' }] })
+            }
             const payload = {
                 user: {
                     id: user.id
@@ -70,4 +66,5 @@ router.post('/', [
 
     });
 
-module.exports = router;
+
+module.exports = router; 
