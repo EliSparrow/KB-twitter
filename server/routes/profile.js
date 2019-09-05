82488e9ccd4134b76ebff8256ade19cec8342424
skens06 <kenny.moutote@coding-academy.fr>
2019-09-06 00:17:30 +0200

    delete profil + config db one line findandmodify

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -80,4 +80,40 @@ router.post('/', [auth, [
     });
 
 
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id })
+            .populate('user', ['username', 'avatar']);
+
+        if (!profile) return res.status(400)
+            .json({ message: 'Profile not found' });
+
+
+        res.json(profile);
+    }
+    catch (err) {
+        console.error(err.message);
+        if (err.kind == 'ObjectifId') {
+            return res.status(400)
+                .json({ message: 'Profile not found' });
+        }
+        res.status(500).send('Server error');
+    }
+});
+
+
+router.delete('/', auth, async (req, res) => {
+    try {
+        await Profile.findOneAndRemove({ user: req.user.id });
+
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ message: 'User deleted' });
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
 module.exports = router;
