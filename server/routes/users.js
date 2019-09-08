47294f3816efb05bcef9856844f0f2a4854fc15b
EliSparow <max.pp@hotmail.fr>
2019-09-09 01:59:19 +0200

    added follow route

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,6 +6,7 @@ const config = require('../config/default.json')
 const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const User = require('../models/user.model');
+const Follow = require('../models/follow.model')
 
 router.post('/', [
     check('username', 'Username is required')
@@ -15,61 +16,95 @@ router.post('/', [
     check('password', 'Password must be 5 or more characters').isLength({ min: 5 }),
     //check('birthdate', 'Enter date in format only').isISO8601()
 ],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        const { username, email, password } = req.body;
+  async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+      }
+      const { username, email, password } = req.body;
 
-        try {
-            let user = await User.findOne({ email });
+      try {
+          let user = await User.findOne({ email });
 
-            if (user) {
-                return res
-                .status(400)
-                .json({ errors: [{ message: 'Email already exist' }] });
-            }
+          if (user) {
+              return res
+              .status(400)
+              .json({ errors: [{ message: 'Email already exist' }] });
+          }
 
-            const avatar = gravatar.url(email, {
-                s: '200',
-                r: 'pg',
-                d: 'mm'
-            })
+          const avatar = gravatar.url(email, {
+              s: '200',
+              r: 'pg',
+              d: 'mm'
+          })
 
-            user = new User({
-                username,
-                email,
-                password,
-                avatar
-            });
+          user = new User({
+              username,
+              email,
+              password,
+              avatar
+          });
 
-            const salt = await bcrypt.genSalt(10);
+          const salt = await bcrypt.genSalt(10);
 
-            user.password = await bcrypt.hash(password, salt);
+          user.password = await bcrypt.hash(password, salt);
 
-            await user.save();
+          await user.save();
 
-            const payload = {
-                user: {
-                    id: user.id
-                }
-            }
+          const payload = {
+              user: {
+                  id: user.id
+              }
+          }
 
-            jwt.sign(payload, config.jwtSecret,
-                { expiresIn: 360000 },
-                (err, token) => {
-                    if (err) throw err;
-                    res.json({ token });
-                });
+          jwt.sign(payload, config.jwtSecret,
+              { expiresIn: 360000 },
+              (err, token) => {
+                  if (err) throw err;
+                  res.json({ token });
+              });
 
-        }
-        catch (err) {
-            console.error(err.message);
-            res.status(500).send('Server error');
-        }
+      }
+      catch (err) {
+          console.error(err.message);
+          res.status(500).send('Server error');
+      }
+});
 
+// @route   users/:id/follow
+// @desc    Follow a user
+// @access  Private
+router.post('/:id/follow', auth, async (req, res) => {
+  try {
+    const follow = await Follow.findOne({ followedId: req.params.id, userId: req.user.id});
 
-    });
+    if (follow) {
+      return res
+      .status(400)
+      .json({ errors: [{ message: 'User already followed' }] });
+    }
+
+    follow = new Follow ({
+      followedId: req.params.id,
+      userId: req.user.id 
+    })
+
+    await follow.save()
+
+    res.json(follow)
+
+  } catch (err) {
+    console.error(err.message);
+    if(err.kind === 'ObjectId'){
+      return res.status(404).json({ msg: 'User not found'})
+    }
+    res.status(500).send('Server Error')
+  }
+});
+    
+// @route   users/:id/unfollow
+// @desc    unFollow a user
+// @access  Private
+
 
 module.exports = router;
