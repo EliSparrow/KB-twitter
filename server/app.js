39f5a6c69049062db229ae1486c9af255405bee9
EliSparow <max.pp@hotmail.fr>
2019-09-08 18:03:19 +0200

    updated

--- server/app.js
+++ server/app.js
@@ -1,14 +1,47 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const mongoose = require("mongoose");
+const app = express();
+
 const connectDB = require('./config/db');
+
+let port = process.env.PORT || 4242;
+
+// connect db
+connectDB();
+
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var allowedOrigins = ['http://localhost:3000'];
+app.use(cors({
+  origin: function(origin, callback) {
+
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if (!origin)
+      return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+
+    return callback(null, true);
+  }
+}));
 
 const users = require('./routes/users');
 const profile = require('./routes/profile');
 const posts = require('./routes/posts');
 const login = require('./routes/login');
-
-const app = express();
-
-connectDB();
 
 app.use(express.json({extended: false}));
 
@@ -19,7 +52,4 @@ app.use('/user/login', login);
 app.use('/profile', profile);
 app.use('/post', posts);
 
-
-const PORT = process.env.PORT || 4242;
-
-app.listen(PORT, () => console.log(`Server strated on port ${PORT}`));
+app.listen(port, () => console.log(`Server strated on port ${port}`));
