0e756be8249f35d140eb59c98f7a2171d19c219b
EliSparow <max.pp@hotmail.fr>
2019-09-09 00:03:11 +0200

    new get routes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -5,6 +5,7 @@ const auth = require('../middleware/login');
 
 const Post = require('../models/post.model')
 const User = require('../models/user.model')
+const Follow = require('../models/follow.model')
 
 // @route   POST posts
 // @desc    Create a post
@@ -75,6 +76,80 @@ router.get('/:id', auth, async (req, res) => {
   }
 });
 
+// @route   GET posts/user_posts/:id
+// @desc    GET user's post
+// @access  Private
+router.get('/user_posts/:id', auth, async (req, res) => {
+  try {
+    const posts = await Post.find({ userId: req.params.id })
+
+    if(!posts) {
+      return res.status(404).json({ msg: 'Posts not found or User not found' })
+    }
+
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    if(err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'User not found' })
+    }
+    res.status(500).send('Server Error')
+  }
+});
+
+// @route   GET posts/follow
+// @desc    GET user's post
+// @access  Private
+// router.get('/follow', auth, async (req, res) => {
+//   try {
+//     const posts = await Follow.find({ userId: req.user.id })
+//       .populate('post')
+
+//     if(!posts) {
+//       return res.status(404).json({ msg: 'Posts not found' })
+//     }
+
+//     res.json(posts);
+//   } catch (err) {
+//     console.error(err.message);
+//     if(err.kind === 'ObjectId') {
+//       return res.status(404).json({ msg: 'User not found' })
+//     }
+//     res.status(500).send('Server Error')
+//   }
+// });
+
+// @route   PUT posts/:id
+// @desc    update post by id
+// @access  Private
+router.put('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if(!post) {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+
+    // Check on user
+    if(post.userId.toString() !== req.user.id){
+      return res.status(401).json({ msg: 'User not authorized'})
+    }
+
+    post.content = req.body.content
+
+    await post.save()
+
+    res.json(post)
+  } catch (err) {
+    console.error(err.message);
+    if(err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+    res.status(500).send('Server Error')
+  }
+});
+
+
 // @route   DELETE posts/:id
 // @desc    delete post by id
 // @access  Private
@@ -119,7 +194,7 @@ router.put('/:id/like', auth, async (req, res) => {
 
     await post.save();
 
-    res.json(post.likes);
+    res.json(post.like);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
